1c6c190ef9e10e57124e2f02371a0f481d05ef52
Maysam <maysam.jadidi@gmail.com>
2018-10-13 12:34:15 +0330

    setup.pu corrections

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
